4cad2f46d3e46972bca38339d4a65440a6492c6d
Mirko Zammartino <zammartino.m@gmail.com>
2021-02-05 19:01:29 +0100

    nothing particular

--- src/components/user/index.jsx
+++ src/components/user/index.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 const User = () => {
   const id = useParams();
-  const currentUser = id ? useSelector((state) => state.users.find((user) => user.id == id)) : null;
+  const currentUser = id
+    ? useSelector((state) => state.users.find((user) => user.id === id))
+    : null;
+  const [email, setEmail] = useState(currentUser?.email);
 
   return (
     <form>
-      <input type="email" >
+      <input type="email" defaultValue={email} onChange={(e) => setEmail(e.currentTarget.value)} />
     </form>
-  )
+  );
 };
+
+export default User;
